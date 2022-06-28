5e1c2ba94a92869d176affb3127d59fcd8cc8349
samyrd309 <samyrd309@gmail.com>
2022-06-28 19:19:24 -0400

    Samuel Charles App Vol 2 Cambio 2

--- __init__.py
+++ __init__.py
@@ -2,4 +2,13 @@ import src.KataTeamProyect.main
 
 program = src.KataTeamProyect.main
 
-program.CreateRange()
+def __init__(opt):
+    match opt:
+        case 1:
+            return
+        case 2:
+            return
+        case 3:
+            return
+        case 4: 
+            return
