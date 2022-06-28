9f1701583e2cd05a38fe3b59345bb7155c4ab360
samyrd309 <samyrd309@gmail.com>
2022-06-28 19:51:24 -0400

    Samuel Charles App Vol2 Cambio 3

--- __init__.py
+++ __init__.py
@@ -1,18 +1,29 @@
-import src.KataTeamProyect.main
+from  src.KataTeamProyect import main
 
-program = src.KataTeamProyect.main
+program = main
 
-program.CreateRange(input())
-program.CreateRange(input())
+
+
+
 def seleccionarOpcion(opt):
     match opt:
         case 1:
-            return program.integer_range_contains()
+            defaultRange =  program.CreateRange(input())
+            InputRange =  program.CreateRange(input())
+            return program.integer_range_contains(defaultRange, InputRange)
         case 2:
-            return program.ContainsRange()
+            defaultRange =  program.CreateRange(input())
+            InputRange =  program.CreateRange(input())
+            return program.ContainsRange(defaultRange, InputRange)
         case 3:
-            return program.endPoints()
+            InputRange =  program.CreateRange(input())
+            return program.endPoints(InputRange)
         case 4: 
+            defaultRange =  program.CreateRange(input())
+            InputRange =  program.CreateRange(input())
             return program.Equals()
             
-            
+def __init__ ():
+    return
+
+print(seleccionarOpcion(input("Ingrese la opcion que desea consultar \n1.Integer Range Contains \n2. Contains Range \n3. EndPoints \n Equals ")))
